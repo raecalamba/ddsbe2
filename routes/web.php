3a5de12cc8d333aa4aac6ceaf899dcc2b6b9ac85
raecalamba <raecalamba@gmail.com>
2021-05-30 20:57:10 +0800

    Changes as of May 30, 2021

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,11 @@
 <?php
-
 /** @var \Laravel\Lumen\Routing\Router $router */
-
 /*
-|--------------------------------------------------------------------------
+|---------------------------------------------------------------------
+-----
 | Application Routes
-|--------------------------------------------------------------------------
+|---------------------------------------------------------------------
+-----
 |
 | Here is where you can register all of the routes for an application.
 | It is a breeze. Simply tell Lumen the URIs it should respond to
@@ -13,12 +13,16 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
-
 $router->group(['prefix' => 'api'], function () use ($router) {
     $router->get('/users',['uses' => 'UserController@getUsers']);
 });
+
+// more simple routes
+$router->get('/users', 'UserController@index'); // get all users records
+$router->post('/users', 'UserController@add'); // create new user record
+$router->get('/users/{id}', 'UserController@show'); // get user by id
+$router->put('/users/{id}', 'UserController@update'); // update user record
+$router->patch('/users/{id}', 'UserController@update'); // update user record
+$router->delete('/users/{id}', 'UserController@delete'); // delete record 
 
 ?>
